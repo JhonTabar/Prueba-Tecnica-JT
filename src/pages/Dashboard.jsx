867e2ab86cf525eec7079365783d0168bce9d575
Jhonatan Tabares <tabaresjhonatan771@gmail.com>
2026-05-27 23:57:58 -0500

    feature: funcionalidad login

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,15 +1,23 @@
-import "./Dashboard.css";
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { useNavigate, Outlet } from "react-router-dom";
+import { getLocalStorage } from "../helpers/local-storage";
 import Sidebar from "../components/Sidebar";
-const Dashboard = () => {
+
+function Dashboard() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!getLocalStorage("user")) navigate("/");
+  }, []);
+
   return (
-    <div className="container-dashboard">
+    <div style={{ display: "flex", height: "100vh" }}>
       <Sidebar />
-      <div>
+      <main style={{ flex: 1, overflowY: "auto", padding: "24px" }}>
         <Outlet />
-      </div>
+      </main>
     </div>
   );
-};
+}
 
 export default Dashboard;
